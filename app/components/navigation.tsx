f1ea1444a47811b1e43195737f98654822135803
LinxPhy <lin.aboagye@gmail.com>
2024-01-30 22:14:59 +0000

    small intro, added title and icon navs

---
+++ app/components/navigation.tsx
@@ -0,0 +1,18 @@
+
+
+function Navigation() {
+
+    return (
+        <nav>
+            <i className="fa-brands fa-linkedin fa-lg"></i>
+            <i className="fa-brands fa-github"></i>
+
+            <span className="material-symbols-rounded">
+                article
+            </span>
+        </nav>
+    )
+
+}
+
+export default Navigation
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="title">
+        <h2>HI, I am Linford.<br></br> Pleased to meet you. </h2>
+      </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <section className="icons">
+        <div className="contact_icons">
+          <span className="material-symbols-rounded">
+            mail
+          </span>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Email
           </p>
-        </a>
-      </div>
+        </div>
+
+        <div className="contact_icons">
+          <span className="material-symbols-rounded">
+            article
+          </span>
+
+          <p>
+            CV
+          </p>
+        </div>
+
+        <div className="contact_icons">
+          <span className="material-symbols-rounded">
+            south
+          </span>
+
+          <p>
+            Contact me
+          </p>
+        </div>
+
+      </section>
+      <section className="description"></section>
+
+
+
+      <section className="about_me"></section>
+      <section className="projects"></section>
+
     </main>
   );
 }
